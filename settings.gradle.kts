08d9edb6a2fa8679b39429a6a60e3b72a8f92dfc
trishin666 <trishin666@yandex-team.ru>
2025-08-18 11:33:53 +0300

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+@file:Suppress("UnstableApiUsage")
+
+rootProject.name = "thewarshipsonline"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+  repositories {
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+    gradlePluginPortal()
+    mavenCentral()
+  }
+}
+
+dependencyResolutionManagement {
+  repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+  }
+}
+
+include(":composeApp")
+include(":server")
+include(":shared")
